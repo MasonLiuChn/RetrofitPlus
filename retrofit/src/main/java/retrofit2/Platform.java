6d79c53042f5f35fade77ffa9c429b559a7a506f
liumeng <liumeng>
2017-07-10 17:42:55 +0800

    升级 retrofit 2.3.0 okhttp 3.8.1

--- retrofit/src/main/java/retrofit2/Platform.java
+++ retrofit/src/main/java/retrofit2/Platform.java
@@ -18,8 +18,12 @@ package retrofit2;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+//import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
+import javax.annotation.Nullable;
+import org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement;
 
 class Platform {
   private static final Platform PLATFORM = findPlatform();
@@ -36,19 +40,19 @@ class Platform {
       }
     } catch (ClassNotFoundException ignored) {
     }
-//    try {
-//      Class.forName("java.util.Optional");
-//      return new Java8();
-//    } catch (ClassNotFoundException ignored) {
-//    }
+    try {
+      Class.forName("java.util.Optional");
+      return new Java8();
+    } catch (ClassNotFoundException ignored) {
+    }
     return new Platform();
   }
 
-   Executor defaultCallbackExecutor() {
+  @Nullable Executor defaultCallbackExecutor() {
     return null;
   }
 
-  CallAdapter.Factory defaultCallAdapterFactory( Executor callbackExecutor) {
+  CallAdapter.Factory defaultCallAdapterFactory(@Nullable Executor callbackExecutor) {
     if (callbackExecutor != null) {
       return new ExecutorCallAdapterFactory(callbackExecutor);
     }
@@ -59,36 +63,36 @@ class Platform {
     return false;
   }
 
-  Object invokeDefaultMethod(Method method, Class<?> declaringClass, Object object,
-       Object... args) throws Throwable {
+  @Nullable Object invokeDefaultMethod(Method method, Class<?> declaringClass, Object object,
+                                       @Nullable Object... args) throws Throwable {
     throw new UnsupportedOperationException();
   }
 
-//  @IgnoreJRERequirement // Only classloaded and used on Java 8.
-//  static class Java8 extends Platform {
+  @IgnoreJRERequirement // Only classloaded and used on Java 8.
+  static class Java8 extends Platform {
 //    @Override boolean isDefaultMethod(Method method) {
 //      return method.isDefault();
 //    }
 //
 //    @Override Object invokeDefaultMethod(Method method, Class<?> declaringClass, Object object,
-//        @Nullable Object... args) throws Throwable {
+//                                         @Nullable Object... args) throws Throwable {
 //      // Because the service interface might not be public, we need to use a MethodHandle lookup
 //      // that ignores the visibility of the declaringClass.
 //      Constructor<Lookup> constructor = Lookup.class.getDeclaredConstructor(Class.class, int.class);
 //      constructor.setAccessible(true);
 //      return constructor.newInstance(declaringClass, -1 /* trusted */)
-//          .unreflectSpecial(method, declaringClass)
-//          .bindTo(object)
-//          .invokeWithArguments(args);
+//              .unreflectSpecial(method, declaringClass)
+//              .bindTo(object)
+//              .invokeWithArguments(args);
 //    }
-//  }
+  }
 
   static class Android extends Platform {
     @Override public Executor defaultCallbackExecutor() {
       return new MainThreadExecutor();
     }
 
-    @Override CallAdapter.Factory defaultCallAdapterFactory( Executor callbackExecutor) {
+    @Override CallAdapter.Factory defaultCallAdapterFactory(@Nullable Executor callbackExecutor) {
       if (callbackExecutor == null) throw new AssertionError();
       return new ExecutorCallAdapterFactory(callbackExecutor);
     }
